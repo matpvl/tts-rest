6042a54a32bb2ae573b39fbc3c16eac5b1712df3
Matija Pavlovic <mpavlovi6@gmail.com>
2024-11-06 19:07:55 +0100

    updated the url patterns

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
 from .views import TicketListView, TicketDetailView
 
 urlpatterns = [
+    # LIST , POST
     path("tickets/", TicketListView.as_view(), name="ticket-list"),
+    # GET, PUT, PATCH, DELETE
     path("tickets/<int:id>/", TicketDetailView.as_view(), name="ticket-detail"),
 ]
